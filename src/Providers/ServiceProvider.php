c318bc4756857b54701ad1772302a100990102b3
Daniel Lopes <danielorkae@outlook.com>
2019-11-03 11:40:15 -0300

    add provider

---
+++ src/Providers/ServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Rits\ModelTranslation\Providers;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/resources/lang/en' => resource_path('lang/en/'),
+            __DIR__ . '/resources/lang/pt_BR' => resource_path('lang/pt_BR/'),
+        ]);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
